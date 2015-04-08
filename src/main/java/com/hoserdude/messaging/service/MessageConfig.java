0feb641b0b8b521a08aafcbed57c4afe6afd84b6
hoserdude <tim@hoserdude.com>
2015-04-07 22:51:15 -0700

    workaround for localhost resolution issue

--- src/main/java/com/hoserdude/messaging/service/MessageConfig.java
+++ src/main/java/com/hoserdude/messaging/service/MessageConfig.java
@@ -1,11 +1,13 @@
 package com.hoserdude.messaging.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.*;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,11 +17,17 @@ public class MessageConfig {
 	public static final String MY_EXCHANGE = "my-exchange";
 	public static final String ROUTING_KEY_WORK = "do.work";
 
-	/**
-	 * Used below for declaring an anonymous queue (one that is auto-generated and deleted when the client dies)
-	 */
-	@Autowired
-	private AmqpAdmin amqpAdmin;
+	final static Logger logger = LoggerFactory.getLogger(MessageConfig.class);
+
+	@Bean
+	public ConnectionFactory connectionFactory() {
+		CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
+		connectionFactory.setUsername("guest");
+		connectionFactory.setPassword("guest");
+		connectionFactory.setVirtualHost("/");
+		connectionFactory.setHost("127.0.0.1"); //The trick to getting it to work with Java 8?
+		return connectionFactory;
+	}
 
 	/**
 	 * Template for sending messages to any Queue
@@ -68,6 +76,7 @@ public class MessageConfig {
 	 */
 	@Bean
 	public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
+		logger.info("Connection properties; host={}; port={}; virtualhost={}", connectionFactory.getHost(), connectionFactory.getPort(), connectionFactory.getVirtualHost());
 		SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
 		factory.setConnectionFactory(connectionFactory);
 		factory.setConcurrentConsumers(3);
